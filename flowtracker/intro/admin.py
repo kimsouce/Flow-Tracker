7d1a2fd2e359dba2c262216b7c203f6d55baadea
Dubbi96 <yrs03001@hanyang.ac.kr>
2021-10-20 22:49:32 +0900

    initialize commit

--- flowtracker/intro/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Question 모델을 관리자에 등록
-# from .models import Question
-
-# admin.site.register(Question)
-# Register your models here.
--- yolov5/login.py
+++
@@ -1,6 +0,0 @@
-import wandb
-api = wandb.Api()
-
-run = api.run("van52/Yolov5/<run_id>")
-run.config["key"] = updated_value
-run.update()
